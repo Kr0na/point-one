77477f0e144707ee4a16d4953b28c6fd1776301e
Alex Gradn <alex@granted.name>
2016-03-22 14:27:55 +0200

    Fix multiple store unsubscribing in listen

--- src/utils/listen.js
+++ src/utils/listen.js
@@ -1,45 +1,43 @@
 /**@flow */
 
 export function listen(store:{listen:Function, getState:Function}, fields:Array<string>, stateGetter:Function = (state => state)):Function {
-  return (Component) => {
-    return class WrappedComponent extends Component {
-      constructor(...options) {
-        super(...options)
-        if (!this.state) {
-          this.state = {}
-        }
+  return Component => class WrappedComponent extends Component {
+    constructor(...options) {
+      super(...options)
+      if (!this.state) {
+        this.state = {}
+      }
+      fields.forEach(key => {
+        this.state[key] = stateGetter(store.getState())[key]
+      })
+      if (!this._listeners) {
+        this._listeners = []
+      }
+    }
+
+    componentDidMount() {
+      super.componentDidMount && super.componentDidMount()
+      this._listeners.push(store.listen(state => {
+        state = stateGetter(state)
+        let
+          newState = {},
+          hasChanges = false
         fields.forEach(key => {
-          this.state[key] = stateGetter(store.getState())[key]
-        })
-        if (!this._listeners) {
-          this._listeners = []
-        }
-      }
-
-      componentDidMount() {
-        super.componentDidMount && super.componentDidMount()
-        this._listeners.push(store.listen(state => {
-          state = stateGetter(state)
-          let
-            newState = {},
-            hasChanges = false
-          fields.forEach(key => {
-            if (this.state[key] !== state[key]) {
-              newState[key] = state[key]
-              hasChanges = true
-            }
-          })
-          if (hasChanges) {
-            this.setState(newState)
+          if (this.state[key] !== state[key]) {
+            newState[key] = state[key]
+            hasChanges = true
           }
+        })
+        if (hasChanges) {
+          this.setState(newState)
+        }
+      }))
+    }
 
-        }))
-      }
-
-      componentWillUnmount() {
-        super.componentWillUnmout && super.componentWillUnmout()
-        this._listeners.forEach(listener => listener())
-      }
+    componentWillUnmount() {
+      super.componentWillUnmout && super.componentWillUnmout()
+      this._listeners.forEach(listener => listener())
+      this._listeners = []
     }
   }
 }
