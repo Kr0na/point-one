2cf1ed2252feb8af4a4b2f913bc709557d46bad9
Alex Grand <alex@granted.name>
2016-05-19 00:40:41 +0300

    fix for reducer replacement

--- src/redux/reduxConverter.js
+++ src/redux/reduxConverter.js
@@ -1,23 +1,29 @@
 /**@flow*/
 import type {StoreInitializer} from '../../flow/types'
 
+function wrapReducer(reducer) {
+  return (state, event, ...other) => {
+    //Redux DevTools subscribe INIT event so we convert it to redux
+    if (event.type == '@@point/INIT') {
+      event.type = '@@redux/INIT'
+    }
+    return reducer(state, event)
+  }
+}
 /**
  * Fix main differences between point-one and redux
  */
 export function reduxConverter(next: StoreInitializer): StoreInitializer {
   return (reducer, initialState, extenders) => {
-    const store = next((state, event, ...other) => {
-      //Redux DevTools subscribe INIT event so we convert it to redux
-      if (event.type == '@@point/INIT') {
-        event.type = '@@redux/INIT'
-      }
-      return reducer(state, event)
-    }, initialState, extenders)
+    const store = next(wrapReducer(reducer), initialState, extenders)
     return {
       ...store,
       subscribe: store.listen,
+      dangerously: {
+        replaceReducer: (reducer, safe) => store.dangerously.replaceReducer(wrapReducer(reducer), safe)
+      },
       //Hot reloading should not trigger warnings
-      replaceReducer: reducer => store.dangerously.replaceReducer(reducer, true)
+      replaceReducer: reducer => store.dangerously.replaceReducer(wrapReducer(reducer), true)
     }
   }
 }
